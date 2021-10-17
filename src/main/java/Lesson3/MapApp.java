2700c0c815a23f64224941d9d700f10dadfdb94a
StarmanSN <nimaevs@gmail.com>
2021-10-17 12:20:26 +0300

    Lesson3

--- src/main/java/Lesson3/MapApp.java
+++ src/main/java/Lesson3/MapApp.java
@@ -12,17 +12,17 @@ public class MapApp {
         //equals
         //hashcode
         Map<String, Integer> map = new HashMap<>();
-        map.put("key", 100);
-        map.put("key", 200);
+        map.put("key", 100); // изначальное значение в 100 в ключе
+        map.put("key", 200); // перезаписали значение
 
-        map.containsKey("key");
-        map.remove("key");
-        map.size();
-        map.getOrDefault("key123", 10); // return default
-        map.get("key"); // return null
+        map.containsKey("key"); // содержание ключа
+        map.remove("key"); // удаление
+        map.size(); // размер
+        map.getOrDefault("key123", 10); // return default если такого объекта нет
+        map.get("key"); // return null если такого объекта нет
         map.isEmpty(); // проверка на пустоту
-        map.values(); // Collection
-        map.keySet(); // set
+        map.values(); // Collection (все значения, которые могут повторяться)
+        map.keySet(); // set (множество, только уникальные значения)
 
         Set<Integer> set = new HashSet<>();
 
