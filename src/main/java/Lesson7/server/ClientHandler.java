ab47da92e78a83ee7df7f10c57160f9f810f33f4
StarmanSN <nimaevs@gmail.com>
2021-11-10 19:49:21 +0300

    Lesson7

---
+++ src/main/java/Lesson7/server/ClientHandler.java
@@ -0,0 +1,106 @@
+package Lesson7.server;
+
+import Lesson7.constants.Constants;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ * Обработчик для конкретного клиента
+ */
+public class ClientHandler {
+
+    private MyServer server;
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private String name;
+
+    public ClientHandler(MyServer server, Socket socket) {
+        try {
+            this.server = server;
+            this.socket = socket;
+            this.in = new DataInputStream(socket.getInputStream());
+            this.out = new DataOutputStream(socket.getOutputStream());
+            new Thread(() -> {
+                try {
+                    authentification();
+                    readMessage();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                } finally {
+                    closeConnection();
+                }
+            }).start();
+
+        } catch (IOException ex) {
+            throw new RuntimeException("Проблемы при создании обработчика");
+        }
+    }
+
+    // /auth login pass
+    private void authentification() throws IOException {
+        while (true) {
+            String str = in.readUTF();
+            if (str.startsWith(Constants.AUTH_COMMAND)) {
+                String[] tokens = str.split("\\s+"); // разбитие строки по пробелам / массив длины 3
+                String nick = server.getAuthService().getNickByLoginAndPass(tokens[1], tokens[2]);
+
+                if (nick != null) {
+                    //Такого ника нет в чате...
+                    //авторизовались
+                    name = nick;
+                    sendMessage(Constants.AUTH_OK_COMMAND + " " + nick);
+                    server.broadcastMessage(nick + " вошел в чат");
+                    server.subscribe(this);
+                    return;
+                } else {
+                    sendMessage("Неверные логин/пароль");
+                }
+            }
+        }
+    }
+
+    public void sendMessage(String message) {
+        try {
+            out.writeUTF(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void readMessage() throws IOException {
+        while (true) {
+            String messageFromClient = in.readUTF();
+            // hint можем получать команды
+
+            System.out.println("Сообщение от " + name + ": " + messageFromClient);
+            if (messageFromClient.equals(Constants.END_COMMAND)) {
+                break;
+            }
+            server.broadcastMessage(name + ": " + messageFromClient);
+        }
+    }
+
+    private void closeConnection() {
+        server.unsubscribe(this);
+        server.broadcastMessage(name + " вышел из чата");
+        try {
+            in.close();
+        } catch (IOException ex) {
+            // ignore
+        }
+        try {
+            out.close();
+        } catch (IOException ex) {
+            // ignore
+        }
+        try {
+            socket.close();
+        } catch (IOException ex) {
+            // ignore
+        }
+    }
+}
---
+++ src/main/java/Lesson7/server/ServerApp.java
@@ -0,0 +1,8 @@
+package Lesson7.server;
+
+//launcher
+public class ServerApp {
+    public static void main(String[] args) {
+        new MyServer();
+    }
+}
