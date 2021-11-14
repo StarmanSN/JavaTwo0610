1ca5bc26fe0d0a949ad878d216ecbab4eb8b5b34
StarmanSN <nimaevs@gmail.com>
2021-11-14 22:11:50 +0300

    Lesson9

---
+++ src/main/java/Lesson9/SimpleBoxApp.java
@@ -0,0 +1,35 @@
+package Lesson9;
+
+public class SimpleBoxApp {
+    public static void main(String[] args) {
+        SimpleBox box1 = new SimpleBox(10);
+        SimpleBox box2 = new SimpleBox(20);
+
+        SimpleBox box3 = new SimpleBox("abcde");
+
+        // Сложить 2 значения из коробок
+        if (box1.getObj() instanceof Integer && box2.getObj() instanceof Integer) {
+            int sum = (Integer) box1.getObj() + (Integer) box2.getObj();
+            System.out.println("Сумма = " + sum);
+        } else {
+            System.out.println("Невозможно посчитать сумму");
+        }
+    }
+
+    // Класс-обертка
+    private static class SimpleBox {
+        private Object obj;
+
+        public SimpleBox(Object obj) {
+            this.obj = obj;
+        }
+
+        public Object getObj() {
+            return obj;
+        }
+
+        public void setObj(Object obj) {
+            this.obj = obj;
+        }
+    }
+}
