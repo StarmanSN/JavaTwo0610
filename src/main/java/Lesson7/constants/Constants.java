d0a79b8932383fcd6a558d66b82b909641c34f71
StarmanSN <nimaevs@gmail.com>
2021-11-21 16:12:08 +0300

    Homework8

--- src/main/java/Lesson7/constants/Constants.java
+++ src/main/java/Lesson7/constants/Constants.java
@@ -26,5 +26,5 @@ public class Constants {
 
     public static final String CLIENT_LIST_COMMAND = "/clients";
 
-    public static final int TIME_OUT = 12000;
+    public static final int TIME_OUT = 120000;
 }
