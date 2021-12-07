cbafc98abd81ce44a9d62f74958fdebd0ac66e82
StarmanSN <nimaevs@gmail.com>
2021-12-07 21:37:13 +0300

    Lesson13

---
+++ src/main/java/Lesson13/LockApp.java
@@ -0,0 +1,30 @@
+package Lesson13;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class LockApp {
+    public static void main(String[] args) {
+        Lock lock = new ReentrantLock();
+        new Thread(() -> {
+            try {
+                lock.lock();
+                // кретическая секция
+
+                if (!lock.tryLock(10, TimeUnit.SECONDS)) {
+                    System.out.println("Не удалось захватить блокировку");
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }).start();
+
+        ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+        readWriteLock.readLock().lock();
+    }
+}
