0e654ce08e1ccecebb7b3ac5b10f2335a4ce2c0e
StarmanSN <nimaevs@gmail.com>
2021-12-11 17:36:15 +0300

    123

--- src/main/java/Lesson7/client/ClientApp.java
+++ src/main/java/Lesson7/client/ClientApp.java
@@ -16,7 +16,6 @@ import java.util.concurrent.Future;
 
 public class ClientApp extends JFrame {
 
-
     private JTextField textField;
     private JTextArea textArea;
 
@@ -132,13 +131,6 @@ public class ClientApp extends JFrame {
         JButton authButton = new JButton("Войти");
         loginPanel.add(authButton, BorderLayout.EAST);
         add(loginPanel, BorderLayout.NORTH);
-
-        JPanel onlineClientsPanel = new JPanel(new BorderLayout());
-        JTextField onlineClientsField = new JTextField();
-        onlineClientsPanel.add(onlineClientsField, BorderLayout.CENTER);
-        JButton refresh = new JButton("Обновить");
-        onlineClientsPanel.add(refresh, BorderLayout.SOUTH);
-        add(onlineClientsPanel, BorderLayout.EAST);
 
         authButton.addActionListener(new ActionListener() {
             @Override
