607fc6e5c58102efaf1689fd3fe6242fbc4d86e8
StarmanSN <nimaevs@gmail.com>
2021-12-19 12:46:53 +0300

    Homework15

---
+++ src/main/java/Lesson15/Homework/SomeClass.java
@@ -0,0 +1,22 @@
+package Lesson15.Homework;
+
+import org.junit.Test;
+
+public class SomeClass {
+
+    @BeforeSuite
+    void beforeSuite() {
+
+    }
+
+    @Test
+    void test() {
+
+    }
+
+    @AfterSuite
+    void afterSuite() {
+
+    }
+
+}
---
+++ src/main/java/Lesson15/Homework/TestClass.java
@@ -0,0 +1,12 @@
+package Lesson15.Homework;
+
+public class TestClass {
+    public static void main(String[] args) {
+        start();
+    }
+
+    public static void start() {
+        Class testClass = SomeClass.class;
+
+    }
+}
