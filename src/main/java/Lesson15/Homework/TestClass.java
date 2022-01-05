1a00995f262f6c81a94525b55acc4c20e41f4aa1
StarmanSN <nimaevs@gmail.com>
2022-01-05 15:29:26 +0300

    Homework15

--- src/main/java/Lesson15/Homework/TestClass.java
+++ src/main/java/Lesson15/Homework/TestClass.java
@@ -1,12 +1,42 @@
 package Lesson15.Homework;
 
+import java.lang.reflect.Method;
+
 public class TestClass {
     public static void main(String[] args) {
-        start();
+        start2(Car.class);
     }
 
-    public static void start() {
-        Class testClass = SomeClass.class;
-
+    public static void start2(Class classObject) {
+        Method[] declaredMethods = classObject.getDeclaredMethods();
+        try {
+            int count = 0;
+            for (Method o : declaredMethods) {
+                if (o.getAnnotation(BeforeSuite.class) != null) {
+                    if (count > 1) throw new RuntimeException();
+                    System.out.println(o);
+                    count++;
+                }
+            }
+            for (int i = 0; i < 11; i++) {
+                for (Method t : declaredMethods) {
+                    if (t.isAnnotationPresent(Test.class) && t.getAnnotation(Test.class).priority() == i) {
+                        Test test = t.getAnnotation(Test.class);
+                        System.out.println(t);
+                        System.out.println("priority: " + test.priority());
+                    }
+                }
+            }
+            int count2 = 0;
+            for (Method o : declaredMethods) {
+                if (o.getAnnotation(AfterSuite.class) != null) {
+                    if (count2 > 1) throw new RuntimeException();
+                    System.out.println(o);
+                    count2++;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
